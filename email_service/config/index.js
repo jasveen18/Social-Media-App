fd37aa96cfa5228af255cb5a259aee68a0c551cc
Jasveen Singh Kohli <71898741+jasveen18@users.noreply.github.com>
2022-01-02 19:04:51 +0530

    Update index.js

--- email_service/config/index.js
+++ email_service/config/index.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 
 const config = {
-    MODE: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3000,
-    EMAIL_USERNAME: process.env.EMAIL_USERNAME,
-    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-    
-    SECRET_KEY: process.env.SECRET_KEY || 'secret_key',
-    JWTEXP: '5d',
+    MODE: process.env.NODE_ENV || "development",
+    PORT: process.env.PORT || 4000,
+    EMAIL: process.env.EMAIL,
+    SECRET: process.env.EMAILPASSWORD,
+    EMAILSERVICE: process.env.EMAILSERVICE,
 
-    DATABASE: process.env.DATABASE,
+    JWTSECRET: process.env.JWTSECRET || "secret",
+    JWTEXP: "5d",
+    BASEURL: process.env.BASEURL || "http://localhost:3000",
+    DBURL: process.env.MONGODBURL,
 };
 
 module.exports = config;
