6f0c75cc24821f98d0c9f9fd5712a4fd6f9e9b42
Jasveen Singh Kohli <71898741+jasveen18@users.noreply.github.com>
2022-01-02 18:54:18 +0530

    Update authController.js

--- server/api/v1/controllers/authController.js
+++ server/api/v1/controllers/authController.js
@@ -1,5 +1,18 @@
+const nodemailer = require('nodemailer');
 const User = require('../models/userModel');
+const tokenManager = require('../helpers/token_manager');
+const userServices = require('../services/userServices');
 const bcrypt = require('bcrypt');
+
+
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
 
 
 // post request for new user signin
@@ -7,22 +20,25 @@ const signin = async(req,res) => {
 
     const { name, username, email, password, confirm_password } = req.body;
 
-    if(!name || !username || !email || !password || !confirm_password ){
+    if(!username || !email || !password || !confirm_password || !name){
         return res.status(422).json({error: 'please fill the fields properly'});
     }
 
     try{
 
-        const emailExist = await User.findOne({email:email});
-        const usernameExist = await User.findOne({username:username});
+        const user = await userServices.findOnebyEmailorUsername(username, email);
+        
+        if(user){
+            if(user.email===email && user.email_verified===true){
+                return res.status(422).json({error: 'email already exist'});
+            }
+            else if(user.username===username){
+                return res.status(422).json({error: 'username already exist'});
+            }
+        }
 
-        if(emailExist){
-            return res.status(422).json({error: 'email already exist'});
-        }
-        else if(usernameExist){
-            return res.status(422).json({error: 'username already exist'});
-        }
-        else if(password != confirm_password){
+        
+        if(password != confirm_password){
             return res.status(422).json({error: 'password is not matching'});
         }
         else if(password.length<6){
@@ -32,11 +48,26 @@ const signin = async(req,res) => {
         {
             const user = new User({username,name,email,password,confirm_password});
             //pre function called
-            const userRegister = await user.save();
+            await user.save();
 
-            if(userRegister){
-                return res.status(200).json(`${user.name} has been successfully registered`);
-            }
+            const verificationToken = tokenManager.newToken(
+                { username: req.username }, 
+                process.env.SECRET_KEY,
+                '1h'
+            );
+
+            const url = `http://localhost:5000/users/email_verify?token=${verificationToken}`;
+
+            transporter.sendMail({
+                to: email,
+                subject: 'Verify Account',
+                html: `Click <a href = '${url}'>here</a> to confirm your email.`
+            });
+
+            return res.status(201).send({
+                message: `Sent a verification email to ${email}`
+              });
+
         }
 
 
@@ -57,24 +88,28 @@ const login = async(req,res) => {
             return res.status(400).json({error:'please fill the details'});
         }
 
-        const userLogin = await User.findOne({username:username});
+        const userLogin = await userServices.findOnebyEmailorUsername(email, username);
 
         if(userLogin){
             const isMatch = await bcrypt.compare(password,userLogin.password);
 
-            // const tokken = await userLogin.generateAuthToken();
+            const loginToken = tokenManager.newToken(
+                { username: req.username },
+                process.env.SECRET_KEY,
+                "1h"
+            );
 
-            // res.cookie('jwtoken', tokken , {
-            //     expire:new Date(Date.now()+25892000000),
-            //     httpOnly:true
-            // });
+            res.cookie('jwtoken', loginToken, {
+                expire: new Date(Date.now()+25892000000),
+                httpOnly: true
+            });
 
             if(!isMatch){
                 res.status(400).json({error:'invalid credientials'});
             }
             else
             {
-                res.status(200).json({message:'user login successfull'});
+                res.status(200).json({message:'user login successfull', token:loginToken});
             }
         }
         else
@@ -88,4 +123,123 @@ const login = async(req,res) => {
 }
 
 
-module.exports = { signin, login };
+//get request for email verification
+const email_verify = (token) => {
+    const decoded = tokenManager.verify(token, process.env.SECRET_KEY);
+    if (decoded.verified === false) {
+      return {
+        status: 401,
+        message: `Email verification failed - ${decoded.content}`,
+      };
+    }
+
+    try {
+      User.findOneAndUpdate(
+        { username: decoded.content.username },
+        { email_verified: true },
+        null,
+        (err, docs) => {
+          if (err) {
+            throw new Error(err);
+          }
+        }
+      );
+
+      ({ username: decoded.content.username }, (err, res) => {
+        User.deleteMany(
+          { email: res.email, email_verified: false },
+          (err) => {
+            if (err) {
+              throw new Error(err);
+            }
+          }
+        );
+      });
+
+    } catch (err) {
+      return {
+        status: 500,
+        message: `Verification failed - ${err.message}`,
+      };
+    }
+    return {
+      status: 200,
+      message: "Email verification successful!",
+    };
+};
+
+
+const forgot_password = async(req,res) => {
+
+    if(req.query.token){
+        const decoded = tokenManager.verify(token, process.env.SECRET_KEY);
+
+        if(decoded.verified === false){
+            return res.status(401).json({message: 'Invalid token'});
+        }
+
+        try{
+
+            const hashedPassword = bcrypt.hashSync(req.body, 10);
+            User.findOneAndUpdate(
+               { username: decoded.content.username },
+               { password: hashedPassword },
+               null,
+               (err, docs) => {
+                    if(err){
+                       throw new Error(err);
+                    }
+                }
+            );
+
+        } catch(err){
+            console.log('error in forgot password', err);
+            return res.status(500).json({message:err});
+        };
+
+    } else{
+
+        try{
+
+            const verificationToken = tokenManager.newToken(
+                { username: req.body.username },
+                process.env.SECRET_KEY,
+                "1h"
+            );
+
+            User.findOne({ username: req.body.username }, (err, res) => {
+                if (err) {
+                  console.log(err);
+                }
+                transporter.sendMail({
+                    address: res.email,
+                    subject: "Password Change Request - Social-Media-App",
+                    body: `Hey ${res.username}!<br><br>
+                            We recieved the request to change your account password.<br>
+                            Click on the link to change your account: 
+                            http://localhost:5000/users/forgot_password?token=${verificationToken}<br><br>
+                            Team Social-Media-App`,
+                });
+                
+            });
+             
+
+        } catch(err){
+            console.log('error in forgot password', err);
+            return res.status(500).json({message: err});
+        }
+
+        return res.status(200).json({message: `Password change email sent to ${req.body.username}`});
+    }
+
+};
+  
+
+const logout = (req,res) => {
+    res.clearCookie('login_token').status(200).json({message: 'User logged out successfully'});
+};
+
+
+
+
+module.exports = { signin, login, email_verify, logout, forgot_password };
