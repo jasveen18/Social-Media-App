c80836f0a6fbe51ed668db33acb9a8c33f181e99
Jasveen Singh Kohli <71898741+jasveen18@users.noreply.github.com>
2022-01-03 19:22:17 +0530

    Update userServices.js

--- server/api/v1/services/userServices.js
+++ server/api/v1/services/userServices.js
@@ -5,11 +5,13 @@ async function findOnebyEmailorUsername(username, email) {
     return await User.findOne({ $or: [{ email }, { username }] }).exec();
 };
 
+
 async function createOne(user) {
     const doc = new User(user);
     await doc.save();
     return doc;
 };
+
 
 async function findOneAndUpdate(filter, update) {
     let doc = await User.findOneAndUpdate(filter, update, {
@@ -18,9 +20,11 @@ async function findOneAndUpdate(filter, update) {
     return doc;
 };
 
+
 async function findOneById(id) {
     return await User.findById(id).exec();
 };
+
 
 async function search(username, email){
     return await User.find({
@@ -28,6 +32,7 @@ async function search(username, email){
             email:{$regex:email, $options:'$i'}
     });
 };
+
 
 async function findOneOnly(id){
     return await User.findOne(id);
