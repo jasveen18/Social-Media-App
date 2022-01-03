8cffd9d3a3c5509fc432a7e7175ce9be97d754f6
Jasveen Singh Kohli <71898741+jasveen18@users.noreply.github.com>
2022-01-03 19:20:56 +0530

    Update authRouter.js

--- server/api/v1/routes/authRouter.js
+++ server/api/v1/routes/authRouter.js
@@ -8,21 +8,19 @@ const router = express.Router();
 
 router.post('/signin', auth.signin);
 
-router.get('/login', auth.login);
-
 router.get('/email_verify', auth.email_verify);
 
-router.post('forgot_password', auth.forgot_password);
+router.post('/forgot_password', auth.forgot_password);
 
 router.post('/logout', authorization, auth.logout);
 
 router.get('/:id', authorization, user.profile);
 
-router.put('/:id', authorization,  user.edit_profile);
+router.put('/:id', authorization, user.edit_profile);
 
 router.delete('/delete/:id', authorization, user.delete_profile);
 
-router.post('/search', user.search_user);
+router.post('/search', authorization, user.search_user);
 
 
 module.exports = router;
